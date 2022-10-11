6b930b72dbbb20693e808086d72e9c91fec66806
MATTHIEU_VICHET <94373989+yougatagaa@users.noreply.github.com>
2022-10-11 22:46:30 +0200

    end of first app with express.js

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
+const { request } = require('express');
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
 
-const name = [{initiale : 'a'}
-]
+
+app.get('/test', (request, response) => {
+    response.json("Hello on the JSON");
+});
+
+
+
 
 
 app.get('/', (request, response) =>{
@@ -20,10 +26,8 @@ app.get('/count', (request, response) => {
     response.sendFile(__dirname + "/count.html");
 })
 
-app.get('/name/:id', (request, response) => {
-    app.listen(port, () => console.log("YOOOOOOOOOOOO"));
-    response.json(name[request.params.id]);
-})
+
+
 
 app.listen(port, () => console.log(`Listening on port :  ${port} \n http://localhost:3000/`));
 
--- public/js/initial.js
+++ public/js/initial.js
@@ -1,11 +1,5 @@
 
-    function initial(){
-        const name = document.getElementById('name').value;
-        const initiale = name[0];
-        switch(initiale){
-            case 'a' :
-                 window.location.href = "/name/0";
-            break;
-        }
-        
+    function initial(){  
+            
+        window.location.href = "/test";
     }
